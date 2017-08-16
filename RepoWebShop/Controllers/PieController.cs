a00899e0f24346b185d964b1676bb2adc84bc00d
Sebastian <sebastian.scd@gmail.com>
2017-08-16 15:57:07 +1200

    First Commit

---
+++ RepoWebShop/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RepoWebShop.Models;
+using RepoWebShop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RepoWebShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel() { Pies = _pieRepository.Pies, CurrentCategory = "Cheese cakes" };
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ RepoWebShop/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RepoWebShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepoWebShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
