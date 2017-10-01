0ef3f827b6e3a531d17348106a607580938ca150
Sebastian <sebastian.scd@gmail.com>
2017-10-02 10:25:40 +1300

    no message

--- RepoWebShop.Tests/WorkingHoursUnitTest.cs
+++ RepoWebShop.Tests/WorkingHoursUnitTest.cs
@@ -51,13 +51,13 @@ namespace RepoWebShop.Tests
 
             estimationHs = 8;
             orderAccreditted = new DateTime(2017, 9, 3, 16, 0, 0); // Domingo 3 de septiembre a las 4pm
-            finishingDate = Helpers.WorkingHours.GetOrderReady(orderAccreditted, estimationHs, _processingHours);
+            finishingDate = WorkingHours.GetOrderReady(orderAccreditted, estimationHs, _processingHours);
             Assert.IsTrue(finishingDate.Year == 2017);
             Assert.IsTrue(finishingDate.Month == 9);
             Assert.IsTrue(finishingDate.Day == 5);
             Assert.IsTrue(finishingDate.Hour == 19);
             Assert.IsTrue(finishingDate.Minute == 59);
-            orderPickup = Helpers.WorkingHours.GetPickUpDate(orderAccreditted, estimationHs, _processingHours, _openHours);
+            orderPickup = WorkingHours.GetPickUpDate(orderAccreditted, estimationHs, _processingHours, _openHours);
             Assert.IsTrue(orderPickup.Year == 2017);
             Assert.IsTrue(orderPickup.Month == 9);
             Assert.IsTrue(orderPickup.Day == 5);
