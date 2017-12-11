afef884958ebc1409d1966fd57a4e85bc9e9f881
Sebastian Cabeza <sebastian.cabeza@outlook.com>
2017-12-11 22:30:12 +1300

    Cumple

--- RepoWebShop/Controllers/ServicesController.cs
+++ RepoWebShop/Controllers/ServicesController.cs
@@ -3,14 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RepoWebShop.Interfaces;
 
 namespace RepoWebShop.Controllers
 {
     public class ServicesController : Controller
     {
+        private readonly IPhotosGalleryRepository _photosRepository;
+        private readonly IPhotosetAlbums _photosetAlbums;
+
+        public ServicesController(IPhotosGalleryRepository photosRepository, IPhotosetAlbums photosetAlbums)
+        {
+            _photosRepository = photosRepository;
+            _photosetAlbums = photosetAlbums;
+        }
+
+
         public ViewResult Birthdays()
         {
-            return View();
+            return View(_photosRepository.GetGalleryPictures());
         }
 
         public ViewResult Breakfast()
