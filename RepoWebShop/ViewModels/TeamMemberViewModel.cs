56055b5bd94f165a778a0fa6ebb5e296eeed316a
Sebastian Cabeza <sebastian.cabeza@outlook.com>
2017-12-21 10:48:45 +1300

    styling

---
+++ RepoWebShop/ViewModels/TeamMemberViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepoWebShop.ViewModels
+{
+    public class TeamMemberViewModel
+    {
+        public string Name { get; set; }
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Email { get; set; }
+    }
+}
