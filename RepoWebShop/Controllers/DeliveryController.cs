5feb51856998362ca5d90638227bb5551af48351
Sebastian Cabeza <sebastian.cabeza@outlook.com>
2018-02-11 00:13:30 +1300

    distance

--- RepoWebShop/Controllers/DeliveryController.cs
+++ RepoWebShop/Controllers/DeliveryController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RepoWebShop.Extensions;
 using RepoWebShop.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -20,8 +22,13 @@ namespace RepoWebShop.Controllers
                 return View(deliveryAddres);
             }
 
-            var apicall = $"http://{Request.Host.ToString()}/api/GetDistance/{deliveryAddres.AddressLine1}";
+            var apicall = $"{Request.HostUrl()}/api/DeliveryData/GetDistance/{deliveryAddres.AddressLine1}";
             var result = await new HttpClient().GetAsync(apicall);
+
+            var body = await result.Content.ReadAsStreamAsync();
+            var DistanceObject = ((JToken)new JsonSerializer().Deserialize<Object>(new JsonTextReader(new StreamReader(body)))).Root;
+            var distance = ((JProperty)((JContainer)DistanceObject.Root).First).Value;
+
 
 
             return View();
