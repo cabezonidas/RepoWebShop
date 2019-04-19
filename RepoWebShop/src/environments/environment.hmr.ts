7d83b6acc44540407e8747b0a430564869224f17
Cabe <sebastian.scd@gmail.com>
2019-04-19 16:20:21 +1200

    added omitted config files

---
+++ RepoWebShop/src/environments/environment.hmr.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    googleMapsApiKey: 'AIzaSyAR5nmTSuiZsjA5Yhgx3w9EDEyF-C8zIwU',
+    production: false,
+    hmr: true,
+    firebase: {
+      apiKey: 'AIzaSyBmyyAkUbYPLALlmPzgSoUcig5scZSexZA',
+      authDomain: 'de-las-artes.firebaseapp.com',
+      databaseURL: 'https://de-las-artes.firebaseio.com',
+      projectId: 'de-las-artes',
+      storageBucket: 'de-las-artes.appspot.com',
+      messagingSenderId: '354698240101'
+    }
+   };
