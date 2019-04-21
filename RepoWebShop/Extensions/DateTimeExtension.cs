3e9e68ad0062d83b1553dd0693fd81c94ac04105
Sebastian Cabeza <sebastian@notedemr.com>
2019-04-21 12:56:09 +1200

    added try catch for when app runs on Unix.

--- RepoWebShop/Extensions/DateTimeExtension.cs
+++ RepoWebShop/Extensions/DateTimeExtension.cs
@@ -9,7 +9,14 @@ namespace RepoWebShop.Extensions
     {
         public static DateTime Zoned(this DateTime time, string timeZoneId)
         {
-            return TimeZoneInfo.ConvertTime(time, TimeZoneInfo.FindSystemTimeZoneById(timeZoneId));
+            try
+            {
+                return TimeZoneInfo.ConvertTime(time, TimeZoneInfo.FindSystemTimeZoneById(timeZoneId));
+            } catch
+            {
+                // Unix systems don't have Argentina time zone
+                return time;
+            }
         }
 		public static bool WithinRange(this DateTime date, DateTime dateFrom, int daysDuration) =>
 				dateFrom <= date && dateFrom.AddDays(daysDuration) >= date;
