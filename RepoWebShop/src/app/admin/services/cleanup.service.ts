8d853301b8dff373ae177636f1bb3539cafee408
Sebastian Cabeza <sebastian@notedemr.com>
2019-05-18 21:59:45 +1200

    added reference to cleanup service api

--- RepoWebShop/src/app/admin/services/cleanup.service.ts
+++ RepoWebShop/src/app/admin/services/cleanup.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface ICleanupData {
+  cartDataRows: number;
+  cartDatesRows: number;
+  cartProductsRows: number;
+  cartLunchesRows: number;
+  lunchesRows: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CleanupService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  cleanSessionData = (entries: number) =>
+    this.http.post('/api/AdminData/ClearUnusedSessionData/' + entries, null) as Observable<ICleanupData>
 }
